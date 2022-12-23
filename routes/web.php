7ad946b30af00d880b5709d687ebc9a1902678eb
prasetyowi <zeroidblack@gmail.com>
2022-12-23 19:04:24 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryKeuanganController;
 use App\Http\Controllers\LaporanKeuanganController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
